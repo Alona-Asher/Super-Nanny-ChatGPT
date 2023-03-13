810356e9f66d99de71f4500efe94915b22099c06
Alona Asher <alonajasher@gmail.com>
2023-03-13 08:51:42 +0200

    adding auxiliary functions

--- app/src/ChatBox.jsx
+++ app/src/ChatBox.jsx
@@ -32,57 +32,61 @@ export const ChatBox = () => {
     }
 
     async function processMsgToChatGPT(chatMessages){
-    let apiMessages = chatMessages.map((messageObj) => {
-        let role = (messageObj.sender === CHATGPT_SENDER) ?
-        "assistant" : "user";
-        return { role: role, content: messageObj.message }      
-    });
+        let apiMessages = getApiMessages(chatMessages);
+        const apiRequestBody = {
+            "model": "gpt-3.5-turbo",
+            "messages": apiMessages
+        }
 
-    const systemMessage = {
-        role: "system",
-        content: "Explain like you are an experienced parents consultant talking to young parents. Be very supportive and caring."
-    }
-    const apiRequestBody = {
-        "model": "gpt-3.5-turbo",
-        "messages": [systemMessage, ...apiMessages]
+        await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+            "Authorization": "Bearer " + API_KEY,
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify(apiRequestBody)
+        }).then((data) => { 
+            return data.json();    
+        }).then((data) => {
+            setMessages(
+            [...chatMessages, {
+                message: data.choices[0].message.content,
+                sender : CHATGPT_SENDER
+            }, 
+            {
+                message: "Anything else you would like to ask me?",
+                sender: CHATGPT_SENDER
+            }])
+            setTyping(false);
+        });
     }
 
-    await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify(apiRequestBody)
-    }).then((data) => { 
-        return data.json();    
-    }).then((data) => {
-        setMessages(
-        [...chatMessages, {
-            message: data.choices[0].message.content,
-            sender : CHATGPT_SENDER
-        }, 
-        {
-            message: "Anything else you would like to ask me?",
-            sender: CHATGPT_SENDER
-        }]
-        )
-        setTyping(false);
-    });
+    function getApiMessages(chatMessages){
+        let apiMessages = chatMessages.map((messageObj) => {
+            let role = (messageObj.sender === CHATGPT_SENDER) ?
+            "assistant" : "user";
+            return { role: role, content: messageObj.message }      
+        });
+
+        const systemMessage = {
+            role: "system",
+            content: "Explain like you are an experienced parents consultant talking to young parents. Be very supportive and caring."
+        }
+        return [systemMessage, ...apiMessages];
     }
+
     return (
         <div style={{ position: "relative", height: "500px", width: "700px"}}>
             <MainContainer>
                 <ChatContainer>
                 <MessageList
                     scrollBehavior='smooth'
-                    typingIndicator={typing ? <TypingIndicator content="Super Nanny is typing"/> : null}
-                    >
+                    typingIndicator={typing ? <TypingIndicator content="Super Nanny is typing"/> : null}>
                     {messages.map((message, i) => {
                     return <Message key={i} model={message} />
                     })}
                 </MessageList>
-                <MessageInput placeholder='Type response here' onSend={handleSend}/>
+                <MessageInput placeholder='Type your question here' onSend={handleSend}/>
                 </ChatContainer>
             </MainContainer>
         </div>
