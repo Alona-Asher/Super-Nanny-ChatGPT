108d8ea46c691fc242260b552f7a0b576cb03872
Alona Asher <alonajasher@gmail.com>
2023-05-01 10:08:46 +0300

    improve styling

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,18 @@
 import { ChatBox } from './ChatBox'
+import './App.css';
 import supermom from './images/supermom.png';
 
 function App() {
   return (
     <div className="App">
-      <div class="header">
-        <h1 style={{color: 'dark blue'}}>Super Nanny Chatbot</h1>
-        <h3 style={{color: 'red'}}>An artificial nanny powered by ChatGPT</h3>
-        <img style={{ position: "relative", height: "100px", width: "100px"}} src={supermom}/>
+      <div className="header">
+        <div className="header-title">
+          <h2>Super Nanny Chatbot</h2>
+        </div>
+        <div className="header-subtitle">
+          <h3>An artificial nanny powered by ChatGPT</h3>
+          <img className="logo1" src={supermom} alt="logo" />
+        </div>
       </div> 
       <ChatBox />
     </div>
