dc470ecc2af76c8b98dc04aae939014f9e3c53f8
Alona Asher <alonajasher@gmail.com>
2023-05-01 10:41:00 +0300

    improve directory hierarchy

---
+++ app/src/Components/App.jsx
@@ -0,0 +1,22 @@
+import { ChatBox } from './ChatBox'
+import '../Styles/App.css';
+import supermom from '../images/supermom.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="header-title">
+          <h2>Super Nanny Chatbot</h2>
+        </div>
+        <div className="header-subtitle">
+          <h3>An artificial nanny powered by ChatGPT</h3>
+          <img className="logo1" src={supermom} alt="logo" />
+        </div>
+      </div> 
+      <ChatBox />
+    </div>
+  )
+}
+
+export default App
